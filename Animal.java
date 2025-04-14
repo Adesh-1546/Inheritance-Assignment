57c193c4b8e04ecaca5b44bd501f67bda2564354
Adesh-1546 <adeshpol1234@gmail.com>
2025-04-14 20:43:38 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,11 @@
+package com.Inheritance.prac;
+
+/* Basic Inheritance
+   Create a class Animal with a method eat(). Inherit a class Dog that has a method bark().
+   Call both methods from the Dog class object.*/
+
+public class Animal {
+	public void eat() {
+		System.out.println("Animal eat food");
+	}
+}
---
+++ Dog.java
@@ -0,0 +1,9 @@
+package com.Inheritance.prac;
+
+public class Dog extends Animal{
+	public void bark() {
+	
+		System.out.println("Dog barks");
+	}
+
+}
